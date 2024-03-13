8d0fb55bdb9481514446b48c2a52133ff2722c19
Monica Rondón <rondonmonica@johndeere.com>
2024-03-14 00:48:07 +0100

    [HEAD] Initial Commit: webber new | webber serve -t pwa -s Service -p 443 --browser safari --browser-self-signed

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "First-webber",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .executable(name: "App", targets: ["App"]),        
+        .executable(name: "Service", targets: ["Service"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swifweb/web", from: "1.0.0-beta.2.0.0")
+    ],
+    targets: [
+        .executableTarget(name: "App", dependencies: [
+            .product(name: "Web", package: "web")
+        ]),        
+        .executableTarget(name: "Service", dependencies: [
+            .product(name: "ServiceWorker", package: "web")
+        ], resources: [
+            //.copy("images/favicon.ico"),
+            //.copy("images")
+        ]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
